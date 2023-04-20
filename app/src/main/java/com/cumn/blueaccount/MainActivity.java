b081ad1b39af5eda6b4a18a1235679f260452ff2
rociovelascor <rocio.velascor@alumnos.upm.es>
2023-04-20 18:17:30 +0200

    authentication?

--- app/src/main/java/com/cumn/blueaccount/MainActivity.java
+++ app/src/main/java/com/cumn/blueaccount/MainActivity.java
@@ -28,7 +28,23 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Arrays;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.cumn.blueaccount.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.BuildConfig;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -37,10 +53,56 @@ public class MainActivity extends AppCompatActivity {
     private Button createButton;
     private EditText inputCantidad, inputFecha, inputDescripcion;
     private AutoCompleteTextView inputEtiqueta;
+    final static String LOG_TAG = "btb";
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private static final int RC_SIGN_IN = 2022;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.authentication);
+        findViewById(R.id.logoutButton).setOnClickListener(v -> {
+            mFirebaseAuth.signOut();
+            Log.i(LOG_TAG, getString(R.string.signed_out));
+        });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user is signed in
+                    CharSequence username = user.getDisplayName();
+                    Toast.makeText(MainActivity.this, getString(R.string.firebase_user_fmt, username), Toast.LENGTH_LONG).show();
+                    Log.i(LOG_TAG, "onAuthStateChanged() " + getString(R.string.firebase_user_fmt, username));
+                    ((TextView) findViewById(R.id.textView)).setText(getString(R.string.firebase_user_fmt, username));
+                } else {
+                    // user is signed out
+                    startActivityForResult(
+                            // Get an instance of AuthUI based on the default app
+                            AuthUI.getInstance().
+                                    createSignInIntentBuilder().
+                                    setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()
+                                    )).
+                                    setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */).
+                                    //setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */).
+                                            build(),
+                            RC_SIGN_IN
+                    );
+
+                    //Remains logged in when unique gmail account
+                    // setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */).
+                    //setIsSmartLockEnabled(BuildConfig.DEBUG /* credentials */, true /* hints */).
+
+                }
+            }
+        };
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -134,5 +196,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //setContentView(binding.getRoot());
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, R.string.signed_in, Toast.LENGTH_SHORT).show();
+                Log.i(LOG_TAG, "onActivityResult " + getString(R.string.signed_in));
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, R.string.signed_cancelled, Toast.LENGTH_SHORT).show();
+                Log.i(LOG_TAG, "onActivityResult " + getString(R.string.signed_cancelled));
+                finish();
+            }
+        }
     }
 }
