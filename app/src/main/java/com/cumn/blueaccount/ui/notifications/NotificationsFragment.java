beab07438444f60f33ead1f645b791ffff8d3feb
sergiodlX <sergiodelacruz2001@outlook.es>
2023-05-10 18:39:16 +0200

    Boton Salir grupo

--- app/src/main/java/com/cumn/blueaccount/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/cumn/blueaccount/ui/notifications/NotificationsFragment.java
@@ -10,12 +10,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
 
 import com.cumn.blueaccount.CambiarGrupoActivity;
 import com.cumn.blueaccount.CreaGrupoActivity;
 import com.cumn.blueaccount.MainActivity;
 import com.cumn.blueaccount.R;
 import com.cumn.blueaccount.databinding.FragmentGruposBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
 
@@ -23,11 +28,11 @@ public class NotificationsFragment extends Fragment {
 
     private FragmentGruposBinding binding;
 
-    private Button creaGrupo, cambiaGrupo;
+    private Button creaGrupo, cambiaGrupo, salirGrupo;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        NotificationsViewModel notificationsViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(NotificationsViewModel.class);
+        NotificationsViewModel notificationsViewModel = new ViewModelProvider((ViewModelStoreOwner) this, (ViewModelProvider.Factory) new ViewModelProvider.NewInstanceFactory()).get(NotificationsViewModel.class);
 
         binding = FragmentGruposBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -40,6 +45,26 @@ public class NotificationsFragment extends Fragment {
 
         cambiaGrupo = root.findViewById(R.id.bCambiaGrupo);
         cambiaGrupo.setOnClickListener(v -> {
+            Intent cambiargrupo = new Intent(NotificationsFragment.this.getContext(), CambiarGrupoActivity.class);
+            startActivity(cambiargrupo);
+        });
+
+        salirGrupo = root.findViewById(R.id.bSalirGrupo);
+        salirGrupo.setOnClickListener(v -> {
+            String grupo = MainActivity.getGrupoActual();
+            FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+            String user = mFirebaseAuth.getCurrentUser().getEmail();
+            FirebaseDatabase database = FirebaseDatabase.getInstance("https://blueaccount-e4707-default-rtdb.europe-west1.firebasedatabase.app");
+            DatabaseReference myRef = database.getReference("/Grupos/"+ grupo + "/Usuarios/" );
+            myRef.get().addOnCompleteListener( task ->{
+                DatabaseReference delUser;
+                for ( DataSnapshot child : task.getResult().getChildren()) {
+                    if(child.getValue().equals(user)){
+                        delUser = child.getRef();
+                        delUser.removeValue();
+                    }
+                }
+            });
             Intent cambiargrupo = new Intent(NotificationsFragment.this.getContext(), CambiarGrupoActivity.class);
             startActivity(cambiargrupo);
         });
